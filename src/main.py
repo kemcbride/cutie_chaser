8fbb31dceb5dd50d57518b25925a76c11aa4d920
Kelly McBride <kemcbride28@gmail.com>
2017-09-29 19:45:36 -0400

    Allllllright, let's rolll.....

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!python3
+# I'm so fuckin' pissed that this is taking way more effort than I wanted.
+
+OAUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+
+def __main__():
+    print("#ThanksGoogle")
+    do_oauth_bullshit()
+    print("Well fingers damn crossed")
